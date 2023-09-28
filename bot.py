b0f18deda6dca398fda4b50b30ab0b56a474166a
Zhuoran Tan <t@zhuorantan.com>
2023-09-28 09:30:44 +0800

    Reduce unnecessary messages

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import logging
 from telegram.ext import ApplicationBuilder, ExtBot
 
 async def __get_status(session: aiohttp.ClientSession, models: set[str], country_code: str, location: str):
-  status = []
+  status = {}
 
   for model in models:
     url = f'https://www.apple.com/{country_code}/shop/fulfillment-messages?pl=true&parts.0={model}&location={location}'
@@ -12,10 +12,15 @@ async def __get_status(session: aiohttp.ClientSession, models: set[str], country
     async with session.get(url) as response:
       data = await response.json()
 
+      stores_data = data['body']['content']['pickupMessage']['stores']
+      if not stores_data:
+        logging.warning(f'No stores found for {model}. data: {data}')
+        continue
+
       productName = None
       stores = []
 
-      for store in data['body']['content']['pickupMessage']['stores']:
+      for store in stores_data:
         storeName = store['storeName']
         part = store['partsAvailability'].get(model)
         if not part:
@@ -25,28 +30,33 @@ async def __get_status(session: aiohttp.ClientSession, models: set[str], country
         if available:
           stores.append(storeName)
 
-      status.append((productName, stores))
+      status[model] = (productName, stores)
 
   return status
 
 async def __start(bot: ExtBot, chat_ids: set[int], models: set[str], country_code: str, location: str):
   session = aiohttp.ClientSession()
-  last_status = None
+  last_status_by_model = {}
 
   while True:
-    status = await __get_status(session, models, country_code, location)
+    status_by_model = await __get_status(session, models, country_code, location)
 
-    if status != last_status:
-      last_status = status
+    for product, status in status_by_model.items():
+      if last_status_by_model.get(product) == status:
+        continue
+
+      product, stores = status
+
       for chat_id in chat_ids:
-        for product, stores in status:
-          if not stores:
-            message = f'❌ *{product}* is not available for pickup'
-          else:
-            store_list = '\n'.join(f'• {store}' for store in stores)
-            message = f'✅ *{product}* is available for pickup at:\n{store_list}'
+        if not stores:
+          message = f'❌ *{product}* is not available for pickup'
+        else:
+          store_list = '\n'.join(f'• {store}' for store in stores)
+          message = f'✅ *{product}* is available for pickup at:\n{store_list}'
 
-          await bot.send_message(chat_id=chat_id, text=message, parse_mode='MarkdownV2')
+        await bot.send_message(chat_id=chat_id, text=message, parse_mode='MarkdownV2')
+
+    last_status_by_model = status_by_model
 
     await asyncio.sleep(20)
 
